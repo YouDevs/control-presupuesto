a142c0b00cde36b009a0b68ddadb97eb871f47cf
Carlos Hernández <carlos_develops@outlook.com>
2023-06-14 09:35:31 -0600

    agregando modal para mostrar formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Modal from './components/Modal'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+
+  const handleNuevoGasto = () => {
+    setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500)
+  }
 
   return (
     <div>
@@ -13,6 +25,18 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+      { isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img
+            src={IconoNuevoGasto}
+            alt="icono nuevo gasto"
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+
+      {modal && <Modal setModal={setModal} animarModal={animarModal} setAnimarModal={setAnimarModal} /> }
+
     </div>
   )
 }
