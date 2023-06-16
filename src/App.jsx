dcae6550de2ee0366086bf1c8cac18a4c7df0d42
Carlos Hernández <carlos_develops@outlook.com>
2023-06-16 10:11:14 -0600

    formulario agregar gasto

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Modal from './components/Modal'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import { generarId } from './helpers'
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
+  const [gastos, setGastos] = useState([])
 
   const handleNuevoGasto = () => {
     setModal(true)
@@ -15,6 +17,17 @@ function App() {
     setTimeout(() => {
       setAnimarModal(true)
     }, 500)
+  }
+
+  const GuardarGasto = gasto => {
+    gasto.id = generarId()
+    setGastos([...gastos, gasto])
+
+    setAnimarModal(false)
+		setTimeout(() => {
+			setModal(false)
+		}, 500)
+
   }
 
   return (
@@ -35,7 +48,13 @@ function App() {
         </div>
       )}
 
-      {modal && <Modal setModal={setModal} animarModal={animarModal} setAnimarModal={setAnimarModal} /> }
+      {modal &&
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+          GuardarGasto={GuardarGasto}
+        />}
 
     </div>
   )
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+import Mensaje from './Mensaje'
 import CerrarBtn from "../img/cerrar.svg";
 
-const Modal = ({ setModal, animarModal, setAnimarModal }) => {
+const Modal = ({ setModal, animarModal, setAnimarModal, GuardarGasto }) => {
+
+
+	const [nombre, setNombre] =  useState('')
+	const [cantidad, setCantidad] =  useState(0)
+	const [categoria, setCategoria] =  useState('')
+	const [mensaje, setMensaje] =  useState('')
+
 	const ocultarModal = () => {
 
 		setAnimarModal(false)
@@ -10,13 +19,76 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
 
 	};
 
+	const handleSubmit = e => {
+		e.preventDefault()
+		if ([nombre, cantidad, categoria].includes('')) {
+			setMensaje('Todos los campos son obligatorios')
+
+			setTimeout( () => {
+				setMensaje('')
+			}, 3000)
+
+			return
+		}
+
+
+		GuardarGasto({nombre, cantidad, categoria})
+	}
+
 	return (
 		<div className="modal">
 			<div className="cerrar-modal">
 				<img src={CerrarBtn} alt="cerrar modal" onClick={ocultarModal} />
 			</div>
-			<form className={`formulario ${animarModal? 'animar': 'cerrar'}`}>
+			<form
+				className={`formulario ${animarModal? 'animar': 'cerrar'}`}
+				onSubmit={handleSubmit}
+			>
 				<legend>Nuevo gasto</legend>
+				{mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+				<div className="campo">
+					<label htmlFor="nombre">Nombre gasto</label>
+					<input
+						id="nombre"
+						type="text"
+						placeholder="Añade el nombre del gasto"
+						value={nombre}
+						onChange={ e => setNombre(e.target.value) }
+					/>
+				</div>
+				<div className="campo">
+					<label htmlFor="cantidad">Cantidad</label>
+					<input
+						id="cantidad"
+						type="number"
+						placeholder="Añade la cantidad del gasto: ej. 300"
+						value={cantidad}
+						onChange={e => setCantidad(Number(e.target.value)) }
+					/>
+				</div>
+				<div className="campo">
+					<label htmlFor="categoria">Categoría</label>
+					<select
+						name=""
+						id="categoria"
+						value={categoria}
+						onChange={e => setCategoria(e.target.value)}
+					>
+						<option value="">-- seleccione --</option>
+						<option value="ahorro">Ahorro</option>
+						<option value="comida">Comida</option>
+						<option value="casa">Casa</option>
+						<option value="gastos">Gastos Varios</option>
+						<option value="ocio">Ocio</option>
+						<option value="salud">Salud</option>
+						<option value="suscripciones">Suscripciones</option>
+					</select>
+				</div>
+
+				<input
+					type="submit"
+					value="Añadir gasto"
+				/>
 			</form>
 		</div>
 	);
