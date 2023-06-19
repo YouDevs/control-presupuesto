0e1170a9ea680c962aa11b51140ed6a92c1f08cb
Carlos Hernández <carlos_develops@outlook.com>
2023-06-19 09:56:11 -0600

    seteando totalGastado y totalDisponible

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-const ControlPresupuesto = ({presupuesto}) => {
+const ControlPresupuesto = ({gastos, presupuesto}) => {
+
+    const [disponible, setDisponible] = useState(0)
+    const [gastado, setGastado] = useState(0)
+
+    useEffect(() => {
+        // reduce: acumulador, lógica, en qué valor inicia
+        const totalGastado = gastos.reduce( (total, gasto) => gasto.cantidad + total, 0 )
+        const totalDisponible = presupuesto - totalGastado
+
+        setDisponible(totalDisponible)
+        setGastado(totalGastado)
+
+    }, [gastos])
+
 
     const formatearCantidad = (cantidad) => {
         return cantidad.toLocaleString('en-US', {
@@ -19,10 +33,10 @@ const ControlPresupuesto = ({presupuesto}) => {
                 <span>Presupuesto:</span> { formatearCantidad(presupuesto) }
             </p>
             <p>
-                <span>Disponible:</span> { formatearCantidad(0) }
+                <span>Disponible:</span> { formatearCantidad(disponible) }
             </p>
             <p>
-                <span>Gastado:</span> { formatearCantidad(0) }
+                <span>Gastado:</span> { formatearCantidad(gastado) }
             </p>
         </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
 import ControlPresupuesto from './ControlPresupuesto'
 
-const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto}) => {
+const Header = ({gastos, presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto}) => {
   return (
     <header>
         <h1>Planificador de gastos</h1>
         {isValidPresupuesto?
             (<ControlPresupuesto
               presupuesto={presupuesto}
+              gastos={gastos}
             />):
             (<NuevoPresupuesto
                 presupuesto={presupuesto}
